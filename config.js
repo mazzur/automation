868e42c9df50516fec8a206048d885d9ddae9b23
Vitalii_Mazur <Vitalii_Mazur@epam.com>
2015-11-14 20:05:58 +0200

    add gulp stylus task

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    paths: {
+        dist: 'dist',
+        html: 'src/index.html'
+    },
+    patterns: {
+        dist: 'dist/**',
+        stylus: 'src/styl/**.styl'
+    }
+};
+
+export default config;
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,24 @@
 import gulp from 'gulp';
+import stylus from 'gulp-stylus';
+import config from './config';
+import rimraf from 'rimraf';
+import sourcemaps from 'gulp-sourcemaps';
+
+gulp.task('stylus', () => {
+    gulp.src(config.patterns.stylus)
+        .pipe(sourcemaps.init())
+        .pipe(stylus())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.paths.dist));
+});
+
+gulp.task('html', () => {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist));
+});
+
+gulp.task('clean', (cb) => {
+    rimraf(config.patterns.dist, cb);
+});
+
+gulp.task('default', ['clean', 'html', 'stylus']);
